5604a27d810809e3cec1fa4108aa1e117c4f6928
qianchen-123 <85061508+qianchen-123@users.noreply.github.com>
2022-02-06 13:21:55 +0100

    Add files via upload

--- pipeline3.vhd
+++ pipeline3.vhd
@@ -29,32 +29,57 @@ end entity pipeline3;
 
 architecture BHV of pipeline3 is
 
-signal valid : std_logic;
-signal data : unsigned(NbData-1 downto 0);
+
 
 signal ready_reg_input :std_logic;
 signal ready_reg_output: std_logic;
 
+signal valid_inter : std_logic;
+signal data_inter : unsigned(NbData-1 downto 0);
+
 begin
 	
-	data <= data_input when (ready_input = '1') else data;
-	valid <= valid_input when (ready_input = '1') else valid;
+
+	
+	
 	ready_reg_input <= ready_input;
 	
 	pipeline3 : process(clock,reset) is
-	
 	begin
 	
 		if reset = '0' then
 		elsif rising_edge(clock) then
-			ready_reg_output <= ready_reg_input;
+		 --accept data if ready is high
+			if(ready_reg_output = '1') then
+				--data_output<=data_input; 
+				--valid_output<= valid_input;
+				--when ds is not ready, accept data into expansion reg until it is valid
+				if ready_reg_input ='0' then
+					data_inter <= data_input;
+					valid_inter <= valid_input;
+				end if;
+				
+			
+			end if;
+			
+			-- when ds becomes ready the expansion reg data is accepted and we must clear the valid register
+			if ready_reg_input = '1' then
+				valid_inter <= '0';
+			end if;
+			
 		end if;	
 	end process pipeline3;
 	
+
 	
-	data_output <= data;
-	valid_output <= valid;
+	--ready as long as there is nothing in the expansion register
+	ready_reg_output <= '1' when valid_inter ='0' else '0';
 	ready_output <= ready_reg_output;
+	
+	--selecting the expansion register if it has valid data
+	valid_output <= valid_input or valid_inter;
+	data_output <= data_inter when valid_inter = '1' else data_input; 
+	
 	
 
 end architecture BHV;
